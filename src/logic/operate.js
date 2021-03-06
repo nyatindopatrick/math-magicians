92a65c3c23cf9f2fedad0155e749ab75deb09333
Pato <nyatindopatrick@gmail.com>
2021-03-06 09:24:47 +0300

    add decimal places

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,15 +3,15 @@ import Big from 'big.js';
 const operate = (numberOne, numberTwo, operation) => {
   switch (operation) {
     case '+':
-      return Big(numberOne).plus(numberTwo).toFixed();
+      return Big(numberOne).plus(numberTwo).toFixed(4);
     case 'X':
-      return Big(numberOne).times(numberTwo).toFixed();
+      return Big(numberOne).times(numberTwo).toFixed(4);
     case '-':
-      return Big(numberOne).minus(numberTwo).toFixed();
+      return Big(numberOne).minus(numberTwo).toFixed(4);
     case '÷':
-      return Big(numberOne).div(numberTwo).toFixed();
+      return Big(numberOne).div(numberTwo).toFixed(4);
     case '%':
-      return Big(numberTwo).div(100).toFixed();
+      return Big(numberTwo).div(100).toFixed(4);
     default:
       return null;
   }
